2fe2b1979a61e08c705360aa102cea5b7bc14a03
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-06-09 06:16:37 -0300

    atualização construtores

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ContaBancaria_1 = require("./ContaBancaria");
-const conta1 = new ContaBancaria_1.ContaBancaria();
-conta1.numero = '01234-5';
-conta1.agencia = '234-5';
-console.log(conta1);
+const conta = new ContaBancaria_1.ContaBancaria();
+console.log(conta);
