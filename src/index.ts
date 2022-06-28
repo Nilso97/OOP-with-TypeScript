1e02495f933f5c17400d55245a818a8b980f2272
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-06-28 06:16:34 -0300

    estudos sobre type predicates no typescript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-import { App } from './app';
+// Type Predicates em TypeScript
 
-new App().server.listen(3000);
+export type MyRequest<B = any, Q = B> = { // Tipos genéricos
+  body?: B;
+  query?: Q;
+};
+
+export type MyUser = {
+  email: string;
+  password: string;
+};
+
+export type Token = {
+  refreshToken: string;
+};
+
+const checkRequest = (request: MyRequest<MyUser, Token>): boolean | never => {
+  if (!keyExists(request, "body")) {
+    throw new Error("Error");
+  }
+
+  if (!keyExists(request, "query")) {
+    throw new Error("Error");
+  }
+
+  const email = request.body.email;
+  const password = request.body.password;
+  const refreshToken = request.query.refreshToken;
+
+  return true;
+};
+
+// Predicado de tipo
+const keyExists = <T extends Record<any, any>, K extends keyof T>(
+  object: T,
+  key: string
+): object is T & Required<Pick<T, K>> => {
+  return key in object;
+};
